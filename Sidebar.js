8b89be87cffc9d67258c0b3bad9878f36e4fcd3c
errant21 <errant21@gmail.com>
2015-05-05 23:38:15 -0400

    Split sidebar into two functions, one to make cards, one triggers on enter that calls make cards.

--- Sidebar.js
+++ Sidebar.js
@@ -1,26 +1,38 @@
-var i = 0;
+var i = i;
 var j = 0;
 
 
+
+
 $(document).ready(
-  function(){
-    $("#classmaker").keypress(function(e) {
-        if (e.which == 13) {
-             if (i==0) {
-                $('#sidebardefault').remove();
-            }
 
-            var current = $("#classmaker").val()
-            $("<div />", { id:"l"+i, class:"card", html: current })
-            .appendTo("#areaOne");
-              $("#classmaker").val("");
+    /* This function will actually make cards */
+    function makecard(name) {
+                //makes the div
+                $("<div />", { id:"l"+i, class:"card", html: current })
+               .appendTo("#areaOne");
 
+               //the custom switch timeline function
+               //This is where I'd load a new timeline
+                 function Switchtimeline(classid) {
+                    alert("Hello! I am an alert box!! " + classid);
+                }
+
+                //actually load the timeline on click
+              document.getElementById("l"+i).onclick = function() {Switchtimeline("l"+i)};
+
+
+                //This makes the class droppable, right now this is
+                //bad coding practice, but it works, although
+                //At this point, we don't have any reason to drop words
+                //I'll comment this out later or change as needed
               $(".class").droppable({
                   drop: function(event, ui) {
                     //ui.draggable.detach().appendTo($(this));
 
                     
                     //* THIS IS THE WORDS thingy
+                    //currently obsolete but harmless
                     var currentclass = "#class" + i;
                     var draggablewords = ui.draggable.html();
                     var currentwords = $(this).html();
@@ -38,7 +50,29 @@ $(document).ready(
                     
                   }
                 });
+            }
 
+  function(){
+    $("#classmaker").keypress(function(e) {
+        if (e.which == 13) {
+             if (i==1) {
+                $('#sidebardefault').remove();
+            }
+
+            
+            
+            
+
+            var current = $("#classmaker").val()
+
+
+            //Insert call to cardmaker here
+            cardmaker("l"+i);
+              
+
+
+                //I have no idea what this hover does.
+                //move it to the cardmaker function if needed
                   $(".class").hover(function(evt){
                           $(this).css('background-color','#B7A8D2');
                     }, function(evt){
@@ -64,6 +98,8 @@ $(document).ready(
                     //         onResize: dragHandler
                     //       });
                     // });
+
+            $("#classmaker").val("");
             i++;
 
            }
