865e305a396c7ac22135bb8d7aed6ab3c5f8c383
Suhas Vijaykumar <suhasv@Suhass-MacBook-Pro.local>
2015-05-06 10:33:05 -0400

    routed everything through mvc. Stuff now works.

--- Sidebar.js
+++ Sidebar.js
@@ -32,6 +32,7 @@ function makecard(name, text) {
         //makes the div
         $("<div />", { id:cardname, class:"class", html: classtext })
        .appendTo("#areaOne");
+
        //makes default card
        $("<div />", { id:"card"+j, class:"card",
         html: "Create more cards near the timeline while this class is selected" })
@@ -53,30 +54,30 @@ function makecard(name, text) {
         //bad coding practice, but it works, although
         //At this point, we don't have any reason to drop words
         //I'll comment this out later or change as needed
-      $(".class").droppable({
-          drop: function(event, ui) {
-            //ui.draggable.detach().appendTo($(this));
+      // $(".class").droppable({
+      //     drop: function(event, ui) {
+      //       //ui.draggable.detach().appendTo($(this));
 
             
-            //* THIS IS THE WORDS thingy
-            //currently obsolete but harmless
-            var currentclass = "#class" + i;
-            var draggablewords = ui.draggable.html();
-            var currentwords = $(this).html();
-            var newdiv = $("<div />", { id:"objective"+i, class:"card", html: draggablewords })
-            $(this).append(newdiv);
-            /*
-            alert(currentwords + "<br>" + draggablewords);
-            $(this).html(currentwords+"<br>"+draggablewords);
-            */
+      //       //* THIS IS THE WORDS thingy
+      //       //currently obsolete but harmless
+      //       var currentclass = "#class" + i;
+      //       var draggablewords = ui.draggable.html();
+      //       var currentwords = $(this).html();
+      //       var newdiv = $("<div />", { id:"objective"+i, class:"card", html: draggablewords })
+      //       $(this).append(newdiv);
+      //       /*
+      //       alert(currentwords + "<br>" + draggablewords);
+      //       $(this).html(currentwords+"<br>"+draggablewords);
+      //       */
 
-            ui.draggable.remove();
+      //       ui.draggable.remove();
 
-            j++;
+      //       j++;
             
             
-          }
-        });
+      //     }
+      //   });
     }
 
 
@@ -91,45 +92,10 @@ $(document).ready(
                 $('#sidebardefault').remove();
             }
 
-            
-            
-            
-
             var current = $("#classmaker").val();
-
             
             //Insert call to cardmaker here
             makecard("l"+i, current);
-              
-
-
-                //I have no idea what this hover does.
-                //move it to the cardmaker function if needed
-                  $(".class").hover(function(evt){
-                          $(this).css('background-color','#B7A8D2');
-                    }, function(evt){
-                          $(this).css('background-color','#fff');
-                    });
-
-                    // $(".class").click(function(evt){
-                    //       $('#resizableTimeline').colResizable({disable:true});
-
-                    //       $(".class").css('border-color','#4850B7');
-                    //       $(this).css('border-color','#EAE853');
-                    //       var lessonNum = $(this).attr('id')[1];
-                    //       console.log('#cardBox' + lessonNum);
-                    //       $('#cardBox' + lessonNum).show();
-                    //       $('#cardBox' + currentTimeLine).hide();
-                          
-                    //       $('#timelineContents'+currentTimeLine).detach();
-                    //       $('#resizableTimeline').append(timelineObjContainer[lessonNum]);
-                    //       $(timelineObjContainer[lessonNum]).show();
-                    //       currentTimeLine = parseInt(lessonNum);
-                    //       $('#resizableTimeline').colResizable({
-                    //         liveDrag:true,
-                    //         onResize: dragHandler
-                    //       });
-                    // });
 
             $("#classmaker").val("");
             i++;
